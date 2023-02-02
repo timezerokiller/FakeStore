7b2abaa480778dad5e667edd4ac416e31fc5c046
timezerokiller <timezerokiller@yandex.ru>
2023-02-02 13:46:21 +0300

    router refactor

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,31 +1,4 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Outlet,
-  Route,
-  Router,
-  RouterProvider,
-} from "react-router-dom";
-import { CategoryPage } from "../pages/category";
-import { HomePage } from "../pages/home";
-import { ResponsiveTemplate } from "../widget/template";
-
-const AppLayout = () => (
-  <>
-    <ResponsiveTemplate />
-  </>
-);
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route element={<AppLayout />}>
-      <Route path="/" element={<HomePage />} />
-      <Route element={<CategoryPage />}>
-        <Route path="category/:categoryName" element={<CategoryPage />} />
-      </Route>
-    </Route>
-  )
-);
+import { RouterProvider } from "react-router-dom";
 
 function App() {
   return <RouterProvider router={router} />;
