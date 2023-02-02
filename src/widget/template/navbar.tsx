b6a5e5b70b11d2df3ccaf97b7d81c251ce5b3714
timezerokiller <timezerokiller@yandex.ru>
2023-02-02 20:50:58 +0300

    refactor

--- src/widget/template/navbar.tsx
+++ src/widget/template/navbar.tsx
@@ -1,36 +1,43 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import CircularProgress from "@mui/material/CircularProgress";
-import Box from "@mui/material/Box";
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemText from '@mui/material/ListItemText'
+import CircularProgress from '@mui/material/CircularProgress'
+import Box from '@mui/material/Box'
 
-import { useGetCategories } from "shared/hooks/useGetCategories";
+import { useGetCategories } from 'shared/hooks/useGetCategories'
 
 export const Navbar = () => {
-  const { data, isError, isLoading } = useGetCategories();
+    const { data, isError, isLoading } = useGetCategories()
 
-  console.log(data, "data");
+    if (isLoading) {
+        return (
+            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                <CircularProgress />
+            </Box>
+        )
+    }
 
-  if (isLoading) {
-    return (
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
-        <CircularProgress />
-      </Box>
-    );
-  }
-
-  if (data) {
-    return data.map((category, index) => (
-      <ListItem key={index} disablePadding>
-        <ListItemButton>
-          <Link to={`/category/${category}`}>{category}</Link>
-        </ListItemButton>
-      </ListItem>
-    ));
-  }
-};
+    if (data) {
+        return (
+            <>
+                <Link to={`/`}>
+                    <ListItem disablePadding>
+                        <ListItemButton>Home</ListItemButton>
+                    </ListItem>
+                </Link>
+                {data.map((category, index) => (
+                    <Link to={`/category/${category}`}>
+                        <ListItem key={index} disablePadding>
+                            <ListItemButton>{category}</ListItemButton>
+                        </ListItem>
+                    </Link>
+                ))}
+            </>
+        )
+    }
+}
