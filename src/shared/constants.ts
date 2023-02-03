1eb817b435eebd402c8d36815bb1e2bcf2506c20
timezerokiller <timezerokiller@yandex.ru>
2023-02-03 09:53:21 +0300

    del domen url

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,4 +1,4 @@
 export const API_URL = import.meta.env.VITE_API_URL
 export const DOMEN_URL = import.meta.env.VITE_DOMEN_URL
-export const PRODUCT_URL = DOMEN_URL + "/product"
-export const CATEGORY_URL = DOMEN_URL + "/category"
+export const PRODUCT_URL = "/product"
+export const CATEGORY_URL = "/category"
